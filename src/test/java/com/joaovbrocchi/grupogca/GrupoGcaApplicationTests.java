770820a529836db68ec99494990413b32afe8e7e
joaovbrocchi <joaobrocchiprofissional@gmail.com>
2024-02-17 11:44:54 -0300

    primeiro commit

---
+++ src/test/java/com/joaovbrocchi/grupogca/GrupoGcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovbrocchi.grupogca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoGcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
